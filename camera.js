e06f20fdf77e27fe1204c400d4c0ab10e8000e49
owata088 <zuro3987563q@yahoo.co.jp>
2019-05-28 12:59:43 +0900

    190528_12

--- camera.js
+++ camera.js
@@ -4,7 +4,7 @@
 カメラで撮影した画像の送信先の記述
 
 */
-let width = 320   // We will scale the photo width to this
+let width = 520   // We will scale the photo width to this
 let height = 0     // This will be computed based on the input stream
 
 let streaming = false
