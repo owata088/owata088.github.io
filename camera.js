b50bdefa6fe6266713be43e9b062fc5c134c6568
owata088 <zuro3987563q@yahoo.co.jp>
2019-06-06 14:54:24 +0900

    190606_01

--- camera.js
+++ camera.js
@@ -4,8 +4,8 @@
 カメラで撮影した画像の送信先の記述
 
 */
-let width = "480"   // We will scale the photo width to this
-let height = "360"     // This will be computed based on the input stream
+let width = "520"   // We will scale the photo width to this
+let height = "720"     // This will be computed based on the input stream
 
 let streaming = false
 
@@ -13,8 +13,9 @@ let video = null
 let canvas = null
 let photo = null
 let startbutton = null
-let constrains = { video: {facingMode: 'environment'}, audio: false }
-let myStream = null;
+let constrains = { video: { facingMode: 'environment' }, audio: false }
+let myStream = null
+let videocanvas = null
 /**
  * ユーザーのデバイスによるカメラ表示を開始し、
  * 各ボタンの挙動を設定する
@@ -24,6 +25,7 @@ function startup() {
     video = document.getElementById('video')
     canvas = document.getElementById('canvas')
     photo = document.getElementById('photo')
+    videocanvas = document.getElementById('videocanvas')
     startbutton = document.getElementById('startbutton')
 
     videoStart()
@@ -34,8 +36,10 @@ function startup() {
 
             video.setAttribute('width', width)
             video.setAttribute('height', height)
-            canvas.setAttribute('width', width)
-            canvas.setAttribute('height', height)
+            //canvas.setAttribute('width', width)
+            //canvas.setAttribute('height', height)
+            videocanvas.setAttribute('height', height)
+            videocanvas.setAttribute('width', width)
             streaming = true
         }
     }, false)
@@ -45,15 +49,15 @@ function startup() {
         takepicture()
         ev.preventDefault()
         //カメラの動作を停止？
-        streaming = true    
-        
-        if(myStream){
-            for(track of myStream.getTracks()) track.stop();
+        streaming = true
+
+        if (myStream) {
+            for (track of myStream.getTracks()) track.stop();
             myStream = null;
-          };
-          video.pause();
-          if("srcObject" in video) video.srcObject = null;
-          else video.src = null;
+        };
+        video.pause();
+        if ("srcObject" in video) video.srcObject = null;
+        else video.src = null;
     }, false);
 }
 
@@ -70,27 +74,7 @@ function videoStart() {
         })
         .catch(function (err) {
             console.log("An error occured! " + err)
-        })
-    
-//#########################線の描画（＊注意原文ままではカメラ撮影画像表示と競合の可能性大
-// videoは非表示にしておく
-video.style.display = 'none';
-// そのまま表示すると鏡像にならないので反転させておく
-//canvas.style.transform = 'rotateY(180deg)';
-
-//var context2 = canvas.getContext('2d');
-
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-navigator.getUserMedia({video: true, audio: false}, function (stream) {
-    video.src = URL.createObjectURL(stream);
-    draw();
-}, function () {});
-
-// videoの映像をcanvasに描画する
-var draw = function () {
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    requestAnimationFrame(draw);
-};       
+        })          
 }
 
 /**
@@ -107,19 +91,19 @@ function takepicture() {
         clearphoto()
     }
 }
-function videoRestartbutton(){
+function videoRestartbutton() {
     navigator.mediaDevices.getUserMedia(constrains)
-    .then(function(stream){
-      if("srcObject" in video) video.srcObject = stream;
-      else video.src = window.URL.createObjectURL(stream);
-      video.onloadedmetadata = function(e){
-        video.play();
-        myStream = stream;
-      };
-    })
-    .catch(function(err){ console.log(err.name + ": " + err.message); });
-  
-  };
+        .then(function (stream) {
+            if ("srcObject" in video) video.srcObject = stream;
+            else video.src = window.URL.createObjectURL(stream);
+            video.onloadedmetadata = function (e) {
+                video.play();
+                myStream = stream;
+            };
+        })
+        .catch(function (err) { console.log(err.name + ": " + err.message); });
+
+};
 //下記はカメラデータの送信部分
 /*
 function send() {
